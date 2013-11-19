aade1c92419d44f987c9e64bb6e63c8f3f5690f6
Vinney Cavallo <vcavallo@gmail.com>
2013-11-19 16:10:19 -0500

    starting on concern tests

--- spec/models/concern_spec.rb
+++ spec/models/concern_spec.rb
@@ -1,18 +1,22 @@
 require 'spec_helper'
 
 describe Concern do
-  pending "is valid with a title and description" do
-    # This is marked as pending because it's a false positive. we have no validation yet.
-    concern = Concern.new(title: "test title", description: "test description")
+  let(concern){Concern.create(
+    title: "Test Title",
+    description: "This is a test description."
+  )}
+  it "is valid with a title and description" do
     expect(concern).to be_valid
   end
 
   it "is invalid without a title" do
-    expect(Concern.new(title: nil, description: "test description")).to have(1).errors_on(:title)
+    concern.title = nil
+    expect(concern).to have(1).errors_on(:title)
   end
 
   it "is invalid without a description" do
-    expect(Concern.new(title: "test title", description: nil)).to have(1).errors_on(:description)
+    concern.description = nil
+    expect(concern).to have(1).errors_on(:description)
   end
 
   it "belongs to a user" do
