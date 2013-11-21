d92e55ec45c97ef2a672c1f429cb5a369c31146a
ahimmelstoss <ahimmelstoss@gmail.com>
2013-11-21 15:41:29 -0500

    user must login to post a new suggestion; modal renders but without refreshing the page it stays on

--- app/assets/javascripts/new_suggestion.js
+++ app/assets/javascripts/new_suggestion.js
@@ -1,5 +1,4 @@
 function postSuggestion(){
-  var data = {"suggestion_id":suggestion_id, "suggestion_url":suggestion_url, "suggestion_description":suggestion_description}
   $.get('/suggestions/new', function(json){
     // the below comes from authorize in app_controller
     if (json.error == 'login'){
@@ -8,8 +7,7 @@ function postSuggestion(){
         postSuggestion();
       });
     } else {
-
-      // take all form data and perform post to suggestions/create
+      window.location = 'suggestions/new'
     }
   })  
 }
@@ -17,6 +15,6 @@ function postSuggestion(){
 $(function(){
   $("#new_suggestion").click(function(event){
     event.preventDefault();
-    postSuggestion($(this).data("suggestion_id", "suggestion_url", "suggestion_description"));
+    postSuggestion();
   })
 })
--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,4 +1,4 @@
-function jsLogin(){
+ function jsLogin(){
   // fire a get request to load the form
   // gets a script in views/sessions that starts with 'new'.. 
   // goes to new.js.erb and runs it
