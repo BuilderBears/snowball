0df00e7a2fe0587ceb65647a461faefb77f855bf
Vinney Cavallo <vcavallo@gmail.com>
2013-11-22 12:21:15 -0500

    static login page working. modals working with hacky alert window. need to use ajax

--- app/assets/javascripts/new_topic.js
+++ app/assets/javascripts/new_topic.js
@@ -6,6 +6,13 @@ function postTopic(){
       $(window).on("login:success", function(){
         postTopic();
       });
+      $(window).on("login:failure", function(){
+        console.log("we triggered login failure")
+        console.log(json)
+        alert("email or password is incorrect");
+        // above alert is a hack, do below:
+        // AJAX-refresh modal with flash message
+      });
     } else {
       window.location = '/topics/new';
     }
